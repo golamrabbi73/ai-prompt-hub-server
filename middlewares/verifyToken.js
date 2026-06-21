92dc8844c36624401009cda0cee810daa9118d90
golamrabbi73 <golamrabbi73hr@gmail.com>
2026-06-21 11:27:14 +0600

    add jwt verification middleware

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+
+  const token = req.headers.authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+module.exports = verifyToken;
